e0b9d99c5d3a086569d3d881bdfcd3b9c657b65f
Dineshkumar <109271210+Dineshid45@users.noreply.github.com>
2022-07-27 10:57:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,12 @@
-circle 
-// class circle{
-//     constructor(radius,color){
-//         this.radius=radius;
-//         this.color=color;
-//     }   
-// }
-// var a=new circle(1.0,"red");
-// console.log(a)
-
-
-
-
-// radius
-// class circle{
-//     constructor(radius,color){
-//         this.radius=radius;
-//         this.color=color;
-// }
-//    getradius( ){
-//     return "the circle radius is" + "this.radius";
-//     }   
-//     set changeradius(modifie name){
-//         this.radius=modifie name;
-//     }
-// }
-// var a=new circle (1.0,"red");
-// a.changeradius=2.0;
-// console.log(a.radius);
-
-
-
-// color 
-// class circle{
-//     constructor(radius,color){
-//         this.radius=radius;
-//         this.color=color;
-// }
-//    getcolor( ){
-//     return "the circle color is" + "this.color";
-//     }   
-//     set changecolor(modifie color){
-//         this.color=modifie color;
-//     }
-// }
-// var a=new circle (1.0,"red");
-// a.changecolors="yellow";
-// console.log(a.color);
-
-
-// AREA 
-// class circle{
-//     constructor(radius,color){
-//         this.radius=radius;
-//         this.color=color;
-//     }   
-//     getarea(){
-//         return pi*this.radius*this.radius;
-//     }
-// }
-
-// var a = new circle(1.0,"red");
-// console.log(a);
-
-// CIRCUMFRENCE
-// class circle{
-//     constructor(radius,color){
-//         this.radius=radius;
-//         this.color=color;
-//     }   
-//     getcircumfrence(){
-//         return 2*pi*this.radius;
-//     }
-// }
-
-// var a = new circle(1.0,"red");
-// console.log(a);  
-
+class movie {
+    constructor (Title,Studio,Rating){
+       this.Title = Title;
+       this.Studio = Studio;
+       this.Rating = Rating;
+    }
+    retname ( ){
+       return "Title","Studio","Rating";
+    }
+   }
+   var a = new movie ("Casino Royale","Eon Production","PG13");
+   console.log(a.retname( )); 
